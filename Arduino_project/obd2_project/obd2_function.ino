bf729663df167a491d796719ccaf201d36bbd9b3
son-engr-kr <son.engr.kr@gmail.com>
2023-06-18 17:56:39 +0900

    Change All Status Serial to function

--- Arduino_project/obd2_project/obd2_function.ino
+++ Arduino_project/obd2_project/obd2_function.ino
@@ -72,12 +72,12 @@ unsigned char pidMidSignificant[] = {
   };
 
 void PrintOBD2Data(char* HEADER, char* CATEGORY, char* pidname, double value){
-  sprintf(buffer,"%s,%s,%s,%d",HEADER,CATEGORY,pidname,value);
+  sprintf(buffer,"%s,%s,%s,%lf",HEADER,CATEGORY,pidname,value);
   Serial.println(buffer);
 }
 
 void PrintOBD2DTC(char* HEADER, char* CATEGORY,char char1, int num2, int num3, int num4, int num5 , double value){
-  sprintf(buffer,"%s,%s,%c%d%d%d%d,%d",HEADER,CATEGORY,
+  sprintf(buffer,"%s,%s,%c%d%d%d%d,%lf",HEADER,CATEGORY,
   char1,num2,num3,num4,num5,value );
   Serial.println(buffer);
 }
@@ -210,200 +210,175 @@ void interpretPIDAndSend(unsigned char* rxBuf){
       case PID_SHORT_TERM_FUEL_TRIM_1:
         {
           int32_t temp =  (100 * rxBuf[3]) / 128 - 100; ///////
-          Serial.print("OBD2____,STATUS,SHORT_TERM_FUEL_TRIM_1,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"SHORT_TERM_FUEL_TRIM_1",temp);
           break;
         }
 
       case PID_LONG_TERM_FUEL_TRIM_1:
         {
           int32_t temp =  (100 * rxBuf[3]) / 128 - 100;////
-          Serial.print("OBD2____,STATUS,LONG_TERM_FUEL_TRIM_1,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"LONG_TERM_FUEL_TRIM_1",temp);
           break;
         }
 
       case PID_SHORT_TERM_FUEL_TRIM_2:
         {
           int32_t temp =  (100 * rxBuf[3]) / 128 - 100;////
-          Serial.print("OBD2____,STATUS,SHORT_TERM_FUEL_TRIM_2,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"SHORT_TERM_FUEL_TRIM_2",temp);
           break;
         }
 
       case PID_LONG_TERM_FUEL_TRIM_2:
         {
           int32_t temp =  (100 * rxBuf[3]) / 128 - 100;////
-          Serial.print("OBD2____,STATUS,LONG_TERM_FUEL_TRIM_2,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"LONG_TERM_FUEL_TRIM_2",temp);
           break;
         }
 
       case PID_INTAKE_MAP:
         {
           uint32_t temp =  rxBuf[3];
-          Serial.print("OBD2____,STATUS,INTAKE_MAP,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"INTAKE_MAP",temp);
           break;
         }
 
       case PID_INTAKE_TEMP:
         {
           int32_t temp =  rxBuf[3] - 40;////
-          Serial.print("OBD2____,STATUS,INTAKE_TEMP,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"INTAKE_TEMP",temp);
           break;
         }
 
       case PID_THROTTLE:
         {
           uint32_t temp =  (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,THROTTLE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"THROTTLE",temp);
           break;
         }
 
       case PID_COMMANDED_EVAPORATIVE_PURGE:
         {
           uint32_t temp =  (100 * rxBuf[3]) / 255;////
-          Serial.print("OBD2____,STATUS,COMMANDED_EVAPORATIVE_PURGE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"COMMANDED_EVAPORATIVE_PURGE",temp);
           break;
         }
 
       case PID_EVAP_SYS_VAPOR_PRESSURE:
         {
           uint32_t temp =  (256 * rxBuf[3] + rxBuf[4]) / 4 ;////
-          Serial.print("OBD2____,STATUS,EVAP_SYS_VAPOR_PRESSURE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"EVAP_SYS_VAPOR_PRESSURE",temp);
           break;
         }
 
       case PID_CATALYST_TEMP_B1S1:
         {
           int32_t temp =  (256 * rxBuf[3] + rxBuf[4]) / 10 - 40 ;////
-          Serial.print("OBD2____,STATUS,CATALYST_TEMP_B1S1,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"CATALYST_TEMP_B1S1",temp);
           break;
         }
 
       case PID_CATALYST_TEMP_B2S1:
         {
           int32_t temp =  (256 * rxBuf[3] + rxBuf[4]) / 10 - 40 ;////
-          Serial.print("OBD2____,STATUS,CATALYST_TEMP_B2S1,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"CATALYST_TEMP_B2S1",temp);
           break;
         }
 
       case PID_CATALYST_TEMP_B1S2:
         {
           int32_t temp =  (256 * rxBuf[3] + rxBuf[4]) / 10 - 40 ;////
-          Serial.print("OBD2____,STATUS,CATALYST_TEMP_B1S2,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"CATALYST_TEMP_B1S2",temp);
           break;
         }
 
       case PID_CATALYST_TEMP_B2S2:
         {
           int32_t temp = (256 * rxBuf[3] + rxBuf[4]) / 10 - 40 ;////
-          Serial.print("OBD2____,STATUS,CATALYST_TEMP_B2S2,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"CATALYST_TEMP_B2S2",temp);
           break;
         }
 
       case PID_AIR_FUEL_EQUIV_RATIO:
         {
           uint32_t temp = 2 * (256 * rxBuf[3] + rxBuf[4]) / 65536 ;////
-          Serial.print("OBD2____,STATUS,AIR_FUEL_EQUIV_RATIO,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"AIR_FUEL_EQUIV_RATIO",temp);
           break;
         }
 
       case PID_RELATIVE_THROTTLE_POS:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,RELATIVE_THROTTLE_POS,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"RELATIVE_THROTTLE_POS",temp);
           break;
         }
 
       case PID_ABSOLUTE_THROTTLE_POS_B:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,ABSOLUTE_THROTTLE_POS_B,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ABSOLUTE_THROTTLE_POS_B",temp);
           break;
         }
 
       case PID_ABSOLUTE_THROTTLE_POS_C:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,ABSOLUTE_THROTTLE_POS_C,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ABSOLUTE_THROTTLE_POS_C",temp);
           break;
         }
 
       case PID_ACC_PEDAL_POS_D:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,ACC_PEDAL_POS_D,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ACC_PEDAL_POS_D",temp);
           break;
         }
 
       case PID_ACC_PEDAL_POS_E:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,ACC_PEDAL_POS_E,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ACC_PEDAL_POS_E",temp);
           break;
         }
 
       case PID_ACC_PEDAL_POS_F:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,ACC_PEDAL_POS_F,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ACC_PEDAL_POS_F",temp);
           break;
         }  
 
       case PID_COMMANDED_THROTTLE_ACTUATOR:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,COMMANDED_THROTTLE_ACTUATOR,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"COMMANDED_THROTTLE_ACTUATOR",temp);
           break;
         }  
 
       case PID_ETHANOL_FUEL:
         {
           uint32_t temp = (100 * rxBuf[3]) / 255 ;////
-          Serial.print("OBD2____,STATUS,PID_ETHANOL_FUEL,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"PID_ETHANOL_FUEL",temp);
           break;
         }
 
       case PID_FUEL_RAIL_PRESSURE:
         {
           uint32_t temp = 10 * (256 * rxBuf[3] + rxBuf[4]);////
-          Serial.print("OBD2____,STATUS,FUEL_RAIL_PRESSURE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"FUEL_RAIL_PRESSURE",temp);
           break;
         }
 
       case PID_FUEL_INJECTION_TIMING:
         {
           int32_t temp =  256 * rxBuf[3] + rxBuf[4];////
-          Serial.print("OBD2____,STATUS,FUEL_INJECTION_TIMING,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"FUEL_INJECTION_TIMING",temp);
           break;
         }
 
       case PID_ENGINE_TORQUE_DEMANDED:
         {
           int32_t temp = (256 * rxBuf[3] + rxBuf[4]) / 128 - 210;////
-          Serial.print("OBD2____,STATUS,ENGINE_TORQUE_DEMANDED,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ENGINE_TORQUE_DEMANDED",temp);
           break;
         }
 
@@ -411,8 +386,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint32_t temp =  256 * rxBuf[3] + rxBuf[4];
           // Serial.print("Engine Reference Torque (Nm) : ");
-          Serial.print("OBD2____,STATUS,ENGINE_REF_TORQUE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ENGINE_REF_TORQUE",temp);
           break;
         }
 
@@ -420,8 +394,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           float temp =  rxBuf[3] - 125;
           // Serial.print("Actual Engine Percent Torque (%) : ");
-          Serial.print("OBD2____,STATUS,ACTUAL_ENGINE_TORQUE_PERCENTAGE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ACTUAL_ENGINE_TORQUE_PERCENTAGE",temp);
           break;
         }
 
@@ -429,8 +402,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint32_t temp = (256 * rxBuf[3] + rxBuf[4])/20;
           // Serial.print("Engine Fuel Rate (L/h) : ");
-          Serial.print("OBD2____,STATUS,ENGINE_FUEL_RATE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ENGINE_FUEL_RATE",temp);
           break;
         }
 
@@ -438,8 +410,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           float temp = rxBuf[3] - 40;
           // Serial.print("Engine Oil Temperature (degC) : ");
-          Serial.print("OBD2____,STATUS,ENGINE_OIL_TEMP,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ENGINE_OIL_TEMP",temp);
           break;
         }
 
@@ -447,8 +418,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           float temp = (100.0f * rxBuf[3]) / 255.0f;
           // Serial.print("Hybrid Battery Percentage (%) : ");
-          Serial.print("OBD2____,STATUS,HYBRID_BATTERY_PERCENTAGE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"HYBRID_BATTERY_PERCENTAGE",temp);
           break;
         }
 
@@ -456,8 +426,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint32_t temp = 256 * rxBuf[3] + rxBuf[4];
           // Serial.print("Time Since Trouble Code Cleared (min) : ");
-          Serial.print("OBD2____,STATUS,TIME_SINCE_CODES_CLEARED,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"TIME_SINCE_CODES_CLEARED",temp);
           break;
         }
 
@@ -465,8 +434,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint32_t temp = 256 * rxBuf[3] + rxBuf[4];
           // Serial.print("Time With MIL (min) : ");
-          Serial.print("OBD2____,STATUS,TIME_SINCE_CODES_CLEARED,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"TIME_SINCE_CODES_CLEARED",temp);
           break;
         }
 
@@ -474,8 +442,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           float temp = rxBuf[3] - 40 ;
           // Serial.print("Ambient Temperature (degC) : ");
-          Serial.print("OBD2____,STATUS,AMBIENT_TEMP,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"AMBIENT_TEMP",temp);
           break;
         }
 
@@ -483,8 +450,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint32_t temp =100 * (256 * rxBuf[3] + rxBuf[4])/255;
           // Serial.print("Absolute Engine Load (%) : ");
-          Serial.print("OBD2____,STATUS,ABSOLUTE_ENGINE_LOAD,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ABSOLUTE_ENGINE_LOAD",temp);
           break;
         }
 
@@ -492,8 +458,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           float temp =  (256.0f * rxBuf[3] + rxBuf[4])/1000.0f;
           // Serial.print("Control Module Voltage (V) : ");
-          Serial.print("OBD2____,STATUS,CONTROL_MODULE_VOLTAGE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"CONTROL_MODULE_VOLTAGE",temp);
           break;
         }
 
@@ -501,8 +466,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint16_t temp =  rxBuf[3];
           // Serial.print("Barometric Pressure (kPa) : ");
-          Serial.print("OBD2____,STATUS,BAROMETRIC_PRESSURE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"BAROMETRIC_PRESSURE",temp);
           break;
         }
 
@@ -510,8 +474,7 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint32_t temp = (256 * rxBuf[3]) + rxBuf[4];
           // Serial.print("Distance Traveled (km) : ");
-          Serial.print("OBD2____,STATUS,DISTANCE,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"DISTANCE",temp);
           break;
         }
 
@@ -519,42 +482,40 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint16_t temp = rxBuf[3];
           // Serial.print("Engine Temperature : ");
-          Serial.print("OBD2____,STATUS,ENGINE_TEMP,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ENGINE_TEMP",temp);
           break;
         }
       
       case PID_FUEL_LEVEL:
         {
           float temp = (100.0f * rxBuf[3])/255.0f;
-          Serial.print("OBD2____,STATUS,FUEL_LEVEL,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"FUEL_LEVEL",temp);
           break;
         }
 
       case PID_EGR_ERROR:
         {
           float temp = (100.0f * rxBuf[3])/ 128.0f - 100;
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"EGR_ERROR",temp);
+
           // Serial.print("EGR Error (%): ");
-          Serial.print("OBD2____,STATUS,EGR_ERROR,");
-          Serial.println(temp, DEC);
           break;
         }
 
       case PID_COMMANDED_EGR:
         {
           float temp = (100.0f * rxBuf[3]) / 255.0f;
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"COMMANDED_EGR",temp);
+
           // Serial.print("Commanded EGR (%): ");
-          Serial.print("OBD2____,STATUS,COMMANDED_EGR,");
-          Serial.println(temp, DEC);
           break;
         }
       case PID_DISTANCE_WITH_MIL:
         {
           uint32_t temp = 256 * rxBuf[3] + rxBuf[4];
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"DISTANCE_TRAVEL_WITH_MIL",temp);
+
           // Serial.print("Distance traveled with MIL on (km): ");
-          Serial.print("OBD2____,STATUS,DISTANCE_TRAVEL_WITH_MIL,");
-          Serial.println(temp, DEC);
           break;
         }
       
@@ -562,91 +523,86 @@ void interpretPIDAndSend(unsigned char* rxBuf){
         {
           uint32_t temp = 256 * rxBuf[3] + rxBuf[4];
           // Serial.print("Drive RunTime (s): ");
-          Serial.print("OBD2____,STATUS,DRIVE_RUNTIME,");
-          Serial.println(temp, DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"DRIVE_RUNTIME",temp);
           break;
         }
 
       case PID_AUX_INPUT:
         {
           uint8_t temp = rxBuf[3];
-          if(rxBuf[3] == 1){
-            // Serial.println("AUX is in use.");
-            Serial.println("OBD2____,STATUS,Aux is in use,1");
-          }
-          else{
-            // Serial.println("AUX is not in use.");
-            Serial.println("OBD2____,STATUS,Aux is not in use,0");
-          } 
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"AUX",temp);
+
+          // if(rxBuf[3] == 1){
+          //   // Serial.println("AUX is in use.");
+          //   Serial.println("OBD2____,STATUS,Aux is in use,1");
+          // }
+          // else{
+          //   // Serial.println("AUX is not in use.");
+          //   Serial.println("OBD2____,STATUS,Aux is not in use,0");
+          // } 
           break;
         }
 
       case PID_MAF_FLOW:
         {
           int16_t temp = (256 * rxBuf[3] + rxBuf[4]) / 4; // 음수값이 있어서 int8_t사용
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"MAP_FLOW",temp);
           // Serial.print("MAP Flow (degC): ");
-          Serial.print("OBD2____,STATUS,MAP_FLOW,");
-          Serial.println(temp, DEC);
           break;
         }
 
       case PID_TIMING_ADVANCE:
         {
           int8_t temp = rxBuf[3]/2 - 64; // 음수값이 있어서 int8_t사용
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"TIMING_ADVANCE",temp);
+
           // Serial.print("Timing Advance (TDC): ");
-          Serial.print("OBD2____,STATUS,TIMING_ADVANCE,");
-          Serial.println(temp, DEC);
           break;
         }
 
       case PID_VEHICLE_SPEED:
         {
-          uint8_t Vehicle_Speed = rxBuf[3];
-          Serial.print("OBD2____,STATUS,VEHICLE_SPEED,");
-          Serial.println(Vehicle_Speed,DEC);
+          uint8_t temp = rxBuf[3];
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"VEHICLE_SPEED",temp);
           break;
         }
       case PID_FUEL_PRESSURE:
         {
-          uint16_t Fuel_Pressure = 3 * rxBuf[3];
+          uint16_t temp = 3 * rxBuf[3];
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"FUEL_PRESSURE",temp);
+
           // Serial.print("Fuel Pressure (kPa): ");
-          Serial.print("OBD2____,STATUS,FUEL_PRESSURE,");
-          Serial.println(Fuel_Pressure,DEC);
           break;
         }
 
       case PID_ENGINE_LOAD:
         {
-          float Engine_Load = ( 100.0f * rxBuf[3] ) / 255.0f;
+          float temp = ( 100.0f * rxBuf[3] ) / 255.0f;
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ENGINE_LOAD",temp);
+
           // Serial.print("Engine Load (%): ");
-          Serial.print("OBD2____,STATUS,ENGINE_LOAD,");
-          Serial.println(Engine_Load,DEC);
           break;
         }
 
       case PID_COOLANT_TEMP:
         {
-          uint8_t temp;
-          temp = rxBuf[3] - 40;
-          Serial.print("OBD2____,STATUS,COOLANT_TEMP,");
-          Serial.println(temp,DEC);
+          uint8_t temp = rxBuf[3] - 40;
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"COOLANT_TEMP",temp);
           break;
         }
 
       case PID_ENGINE_RPM:
         {
-          uint16_t rpm = ((256 * rxBuf[3]) + rxBuf[4]) / 4;
+          uint16_t temp = ((256 * rxBuf[3]) + rxBuf[4]) / 4;
           // Serial.print("Engine Speed (rpm): ");
-          Serial.print("OBD2____,STATUS,RPM,");
-          Serial.println(rpm,DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"RPM",temp);
           break;
         }
       case PID_ODOMETER:
         {
           uint32_t temp = ((256*256*256 * rxBuf[3])+(256*256 * rxBuf[4])+(256 * rxBuf[5]) + rxBuf[6]) / 10;
           // Serial.print("Engine Speed (rpm): ");
-          Serial.print("OBD2____,STATUS,ODOMETER,");
-          Serial.println(temp,DEC);
+          PrintOBD2Data(OBD2_HEADER,OBD2_CATEGORY_STATUS,"ODOMETER",temp);
           break;
         }
 
